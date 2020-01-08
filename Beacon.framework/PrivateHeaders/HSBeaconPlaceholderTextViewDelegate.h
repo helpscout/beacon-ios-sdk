9f7299f597dab1a7abbfbc9e9bbb81e8030909c0
Sam Symons <sam@helpscout.com>
2020-01-08 08:07:52 -0800

    Beacon framework update

--- Beacon.framework/PrivateHeaders/HSBeaconPlaceholderTextViewDelegate.h
+++ Beacon.framework/PrivateHeaders/HSBeaconPlaceholderTextViewDelegate.h
@@ -6,13 +6,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property(nonatomic, nonnull) NSString *placeholder;
 @property(nonatomic, weak, nullable) id<UITextViewDelegate> wrappedDelegate;
-@property(nonatomic) BOOL usingPlaceholder;
 
 - (instancetype _Nullable)initWithPlaceholder: (NSString * _Nonnull)placeholder;
 
 - (instancetype _Nullable)initWithPlaceholder:(NSString *_Nonnull)placeholder textView:(UITextView *_Nonnull)textView;
 
 - (void)checkForPlaceholder:(UITextView *)textView;
+- (BOOL)isDisplayingPlaceholderInTextView:(UITextView *)textView;
 
 @end
 
--- Beacon.framework/PrivateHeaders/HSButton.h
+++ Beacon.framework/PrivateHeaders/HSButton.h
@@ -1,10 +1,3 @@
-//
-//  HSButton.h
-//  Beacon-Source
-//
-//  Created by Scott Petit on 10/11/19.
-//
-
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
